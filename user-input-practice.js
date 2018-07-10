b18b9e0c67978c0f089a41edcc4fe844eefe790c
Keily Perez <keilyperez778@gmail.com>
2018-07-10 17:00:39 -0400

    lesson 4
    lesson

--- user-input-practice.js
+++ user-input-practice.js
@@ -3,15 +3,17 @@
 
 // 1. Ask the user to enter a number, and print out "The square root of ___ is ___ ", but with the blanks filled in correctly. (Not sure how to get the square root of a number in JavaScript? Google it!)
      let readline = require("readline-sync")
-     let number = readline.question("Please enter your number: ");
-     console.log ("The square root of " + number +"is"+ Math.sqrt (number));
+     let number = readline.question(" Please enter your number: ");
+     console.log (" The square root of " + number +" is "+ Math.sqrt (number));
 // 2. Ask the user to enter their name, and print out "Your name has ___ letters", but with the blank filled in. Not sure how to get the number of letters in a string in JavaScript? Google it!
      let name = readline.question("please enter your name: ");
-     console.log ("Your name has "  + name.length );
+     console.log (" Your name has "  + name.length );
 // 3. Ask the user to enter any word, and print out "The word ____ starts with the letter ___", but with the blanks filled in. Not sure how to get the first letter of a string in JavaScript? ...
      let word = readline.question("please enter any word ");
-     console.log ("The word " + word + "starts with the letter " + word.charAt(0));
+     console.log (" The word " + word + " starts with the letter " + word.charAt(0));
 // 4. Ask the user to enter two numbers, and print out
  // "The average of ___ and ___ is ____", but with the blanks filled in.
-      let numbers = readline.question(" please enter two numbers ");
-      console.log ("The average of " + numbers " and "  + numbers  "is " Math.max(0));
+      let onenumber = readline.question("please enter one number ");
+      let twonumber = readline.question("plese enter another number two ");
+      let sum  = parseInt(onenumber) + parseInt(twonumber) ;
+      console.log ("The average of " + onenumber + " and "  + twonumber + " is " + sum / 2);
